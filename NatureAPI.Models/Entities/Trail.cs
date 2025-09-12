08fec28898d8931174f52f0a42281c4fc79671ae
PabloAlonsoRomero <pabloealonsor@gmail.com>
2025-09-12 00:20:42 -0600

    Creacion de entidad Trail

---
+++ NatureAPI.Models/Entities/Trail.cs
@@ -0,0 +1,13 @@
+namespace NatureAPI.Models.Entities;
+
+public class Trail
+{
+    public int Id { get; set; }
+    public int PlaceId { get; set; }
+    public string Name { get; set; }
+    public double DistanceKm { get; set; }
+    public int EstimatedTimeMinutes { get; set; }
+    public string Difficulty { get; set; }
+    public string Path { get; set; }
+    public bool IsLoop { get; set; }
+}
