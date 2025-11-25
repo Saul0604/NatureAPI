2cbd287b3438a453a198d78d3495c9e881a82748
Saul Alberto Guadarrama Delgdado <sagd08@gmail.com>
2025-11-25 14:30:24 -0600

    prueba deploy

--- NatureAPI/Controllers/PlacesController.cs
+++ NatureAPI/Controllers/PlacesController.cs
@@ -262,7 +262,7 @@ namespace NatureAPI.Controllers
 
             return CreatedAtAction(nameof(GetPlace), new { id = place.Id }, resultDto);
         }
-        
+        [HttpGet("prueba deploy")]
 
         [HttpGet("ai-analyze")]
         public async Task<IActionResult> AnalyzePlacesWithAI([FromQuery] int? placeId)
